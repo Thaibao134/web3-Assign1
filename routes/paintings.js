b64ddbc8fec04172116a2291003f838a6a23b8c6
Thai Bao <tbao981@mtroyal.ca>
2025-03-12 15:28:27 -0600

    updating painting/genre:ref api

--- routes/paintings.js
+++ routes/paintings.js
@@ -202,7 +202,7 @@ router.get('/paintings/genre/:genreId', async (req, res) => {
     try {
         const { data, error } = await req.app.get('supabase')
             .from('paintings')
-            .select(`paintingId, title, yearOfWork, paintinggenres!inner ()`)
+            .select(`paintingId, title, yearOfWork, imageFileName, paintinggenres!inner ()`)
             .eq('paintinggenres.genreId', genreId)
             .order('yearOfWork', {ascending: true})
         
